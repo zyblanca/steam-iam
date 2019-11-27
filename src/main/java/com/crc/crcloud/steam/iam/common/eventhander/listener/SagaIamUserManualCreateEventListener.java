d586343d8114b0452a1b2e5fceb3596d579beaf0
yangtian <yangtian28@crc.com.hk>
2019-11-27 14:39:07 +0800

    增加iam用户启用停用用户Saga事件

--- src/main/java/com/crc/crcloud/steam/iam/common/eventhander/listener/SagaIamUserManualCreateEventListener.java
+++ src/main/java/com/crc/crcloud/steam/iam/common/eventhander/listener/SagaIamUserManualCreateEventListener.java
@@ -16,6 +16,7 @@ import io.choerodon.asgard.saga.producer.StartSagaBuilder;
 import io.choerodon.asgard.saga.producer.TransactionalProducer;
 import io.choerodon.core.exception.CommonException;
 import io.choerodon.core.iam.ResourceLevel;
+import io.choerodon.core.oauth.CustomUserDetails;
 import io.choerodon.core.oauth.DetailsHelper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +28,7 @@ import javax.validation.constraints.NotNull;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static com.crc.crcloud.steam.iam.common.utils.SagaTopic.User.USER_CREATE;
 
@@ -44,54 +46,45 @@ public class SagaIamUserManualCreateEventListener implements ApplicationListener
     @Autowired
     private TransactionalProducer producer;
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     public SagaIamUserManualCreateEventListener(){
         log.info("已注册创建用户事件-发送saga事件");
     }
-//    @Value("${choerodon.devops.message:false}")
-    /**
-     * 注解 @ConditionalOnProperty(prefix = "choerodon.devops",value = "message", havingValue = "true")
-     * 如果 choerodon.devops.message 在配置文件中不是 true ，这个 bean 就不会被加载
-     * 如果 类被加载 onApplicationEvent() 方法必须执行
-     */
-    private boolean devopsMessage = true;
-
-    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Saga(code = USER_CREATE, description = "steam-iam创建用户", inputSchemaClass = UserEventPayload.class)
     @Override
     public void onApplicationEvent(IamUserManualCreateEvent event) {
         @NotNull IamUserDTO user = event.getSource();
         final String logTitle = StrUtil.format("用户[{}|{}]", user.getId(), user.getLoginName());
-        if (devopsMessage){
-            try {
-                UserEventPayload payload = UserEventPayload.builder()
-                        .userId(user.getId())
-                        .realName(user.getRealName())
-                        .loginName(user.getLoginName())
-                        .email(user.getEmail())
-                        .organizationId(user.getCurrentOrganizationId())
-                        .fromUserId(DetailsHelper.getUserDetails().getUserId())
-                        .isLdap(user.getIsLdap())
-                        .build();
-                log.info("{};开始发送Saga事件[{code:{}}],内容: {}", logTitle, USER_CREATE, JSONUtil.toJsonStr(payload));
-                ArrayList<UserEventPayload> payloads = CollUtil.newArrayList(payload);
-                String input = objectMapper.writeValueAsString(payloads);
-                producer.applyAndReturn(
-                    StartSagaBuilder
-                            .newBuilder()
-                            .withLevel(ResourceLevel.ORGANIZATION)
-                            .withSourceId(user.getCurrentOrganizationId())
-                            .withSagaCode(USER_CREATE),
-                        builder -> {
-                            builder.withPayloadAndSerialize(payloads)
-                                    .withRefType("user") // iam-service 中设置为 user
-                                    .withRefId(user.getId().toString());
-                            return input;
-                        });
-            } catch (Exception e){
-                throw new CommonException("error.sagaEvent.organizationUserService.createUserByManual", e);
-            }
+        try {
+            Long fromUserId = Optional.ofNullable(DetailsHelper.getUserDetails()).map(CustomUserDetails::getUserId).orElse(null);
+            UserEventPayload payload = UserEventPayload.builder()
+                    .userId(user.getId())
+                    .realName(user.getRealName())
+                    .loginName(user.getLoginName())
+                    .email(user.getEmail())
+                    .organizationId(user.getCurrentOrganizationId())
+                    .fromUserId(fromUserId)
+                    .isLdap(user.getIsLdap())
+                    .build();
+            log.info("{};开始发送Saga事件[{code:{}}],内容: {}", logTitle, USER_CREATE, JSONUtil.toJsonStr(payload));
+            ArrayList<UserEventPayload> payloads = CollUtil.newArrayList(payload);
+            String input = objectMapper.writeValueAsString(payloads);
+            producer.applyAndReturn(
+                StartSagaBuilder
+                        .newBuilder()
+                        .withLevel(ResourceLevel.ORGANIZATION)
+                        .withSourceId(user.getCurrentOrganizationId())
+                        .withSagaCode(USER_CREATE),
+                    builder -> {
+                        builder.withPayloadAndSerialize(payloads)
+                                .withRefType("user") // iam-service 中设置为 user
+                                .withRefId(user.getId().toString());
+                        return input;
+                    });
+        } catch (Exception e){
+            throw new CommonException("error.sagaEvent.organizationUserService.createUserByManual", e);
         }
     }
 
@@ -102,7 +95,7 @@ public class SagaIamUserManualCreateEventListener implements ApplicationListener
     public List<UserEventPayload> testSagaTask(String data) throws IOException {
         JavaType javaType = objectMapper.getTypeFactory().constructCollectionType(List.class, UserEventPayload.class);
         List<UserEventPayload> payloads = objectMapper.readValue(data, javaType);
-        log.info("SagaTask:[{}],date:[{}]", "testSagaTask", data);
+        log.info("测试SagaTask的Code:[{}], 测试SagaTask的入参date:[{}]", "testSagaTask", data);
         return  payloads;
     }
 
