e2771c3b026117007ff9639f923a21005b6058bb
liuchun <hand-161@crc.com.hk>
2019-12-09 10:32:42 +0800

    feign迁移 查询项目人员

--- src/main/java/com/crc/crcloud/steam/iam/web/IamUserController.java
+++ src/main/java/com/crc/crcloud/steam/iam/web/IamUserController.java
@@ -81,7 +81,7 @@ public class IamUserController {
     //简易权限，后续需要根据实际情况做校验
     @Permission(level = ResourceLevel.PROJECT)
     @ApiOperation(value = "项目人员列表", notes = "项目人员列表", response = IamUserVO.class)
-    @GetMapping("/projects/{project_id}/iam_user")
+    @GetMapping("/projects/{project_id}/users")
     public ResponseEntity<IPage<IamUserVO>> pageProjectUser(@ApiParam(value = "项目ID", required = true)
                                                             @PathVariable(name = "project_id") Long projectId,
                                                             UserSearchDTO userSearchDTO,
