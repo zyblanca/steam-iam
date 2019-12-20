fe6bcf40890036e6ae33d892159e3c7006d01d5d
liuyang <hand-196@crc.com.hk>
2019-12-20 11:45:31 +0800

    fix 同步权限延迟10秒，让iam-service先执行，确保后执行，不然有可能会被iam-service删除掉差异化内容

--- src/main/java/com/crc/crcloud/steam/iam/common/eventhander/ParsePermissionListener.java
+++ src/main/java/com/crc/crcloud/steam/iam/common/eventhander/ParsePermissionListener.java
@@ -1,5 +1,7 @@
 package com.crc.crcloud.steam.iam.common.eventhander;
 
+import cn.hutool.core.date.DateUnit;
+import cn.hutool.core.thread.ThreadUtil;
 import com.crc.crcloud.steam.iam.service.ParsePermissionService;
 import io.choerodon.eureka.event.AbstractEurekaEventObserver;
 import io.choerodon.eureka.event.EurekaEventPayload;
@@ -21,6 +23,8 @@ public class ParsePermissionListener extends AbstractEurekaEventObserver {
 
     @Override
     public void receiveUpEvent(EurekaEventPayload payload) {
+        //延迟10秒，让iam-service先执行，确保后执行，不然有可能会被iam-service删除掉差异化内容
+        ThreadUtil.safeSleep(DateUnit.SECOND.getMillis() * 10);
         parsePermissionService.parser(payload);
     }
 
